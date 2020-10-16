bcb05d8a9c46d59b71048a3da4825e6724d8613e
Acamaeda <acamaeda@gmail.com>
2020-10-16 18:46:54 -0400

    Updated version

--- js/game.js
+++ js/game.js
@@ -16,9 +16,9 @@ let modInfo = {
 
 // Set your version in num and name, but leave the tmt values so people know what version it is
 let VERSION = {
-	num: "2.0.4",
+	num: "2.0.5",
 	name: "Pinnacle of Achievement Mountain",
-	tmtNum: "2.0.4",
+	tmtNum: "2.0.5",
 	tmtName: "Pinnacle of Achievement Mountain"
 }
 
